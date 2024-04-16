cbd35980da0a5dbdd608e1a8ccedf4184576031d
Réda Hamza <reda.hamza@collegemv.qc.ca>
2024-04-16 14:40:22 -0400

    Revert "2 new components/"
    This reverts commit 4854ec231bc3856406669d0d7db84f92a9c82de1.

--- toto/src/components/About.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function About() {
-    return (
-        <div>About 4562216666666</div>
-    )
-}
-
-export default About
--- toto/src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Home() {
-    return (
-        <div>Home V1 </div>
-    )
-}
-
-export default Home
